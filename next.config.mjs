322276539279d9b598d2847ee02a4426e1e1d6f6
xayed007 <xayedhamid@gmail.com>
2026-04-20 21:10:10 +0600

    fix: force build root to process.cwd to avoid desktop conflict

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    outputFileTracingRoot: process.cwd(),
   },
 }
 
